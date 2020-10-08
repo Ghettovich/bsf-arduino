9a4f2f14a659bb4e16ea4a111daa28fee67a74e6
Ghettovich <mrfunkyman@pm.me>
2020-10-09 01:40:22 +0200

    added enum for requested reply for host.

---
+++ src/ReplyEnum.h
@@ -0,0 +1,23 @@
+#ifndef BSF_ARDUINO_REPLYENUM_H
+#define BSF_ARDUINO_REPLYENUM_H
+
+enum ReplyWithCode { FULL_STATE_RPLY, RELAY_STATE_RPLY, DETECTION_STATE_RPLY };
+
+enum ReplyWithCode identifyReplyCode(int replyStateCode);
+
+enum ReplyWithCode identifyReplyCode(int replyStateCode) {
+
+    switch (replyStateCode) {
+        case FULL_STATE_RPLY:
+            return FULL_STATE_RPLY;
+        case RELAY_STATE_RPLY:
+            return RELAY_STATE_RPLY;
+        case DETECTION_STATE_RPLY:
+            return DETECTION_STATE_RPLY;
+        default:
+            return FULL_STATE_RPLY;
+
+    }
+}
+
+#endif // BSF_ARDUINO_REPLYENUM_H
